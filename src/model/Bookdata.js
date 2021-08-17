a87ce65868d31a55b581a050554583cf715c6e82
Bhagitha Paramesh <bhagithaparamesh.techie@gmail.com>
2021-08-17 17:38:05 +0530

    Initial commit

---
+++ src/model/Bookdata.js
@@ -0,0 +1,25 @@
+const mongoose= require('mongoose');//accessing mongoos package
+
+mongoose.connect('mongodb://localhost:27017/libraryDb',
+{
+    useNewUrlParser: true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+});// connection string
+
+//console.log(mongoose.connection.readyState)
+
+const Schema = mongoose.Schema;//schema definition
+const BookSchema = new Schema({
+    title: String,
+    author: String,
+    genre: String,
+    image: String
+     //imag will be saved with fil
+},{strict:false});
+
+//model creation
+var Bookdata = mongoose.model('bookdata',BookSchema);
+
+module.exports = Bookdata;
